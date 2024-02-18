4ada079fd4fb23a1a117c318e65b012bfc28665d
kgf-cmd <Karolfracek123@gmail.com>
2024-02-18 15:54:46 +0100

    adding tiles to game screen

--- singletons/image_manager.gd
+++ singletons/image_manager.gd
@@ -1,5 +1,13 @@
 extends Node
 
+const FRAME_IMAGES: Array = [
+	preload("res://assets/frames/blue_frame.png"),
+	preload("res://assets/frames/red_frame.png"),
+	preload("res://assets/frames/yellow_frame.png"),
+	preload("res://assets/frames/green_frame.png")
+]
+
+
 var _item_images: Array = []
 
 # Called when the node enters the scene tree for the first time.
@@ -36,4 +44,12 @@ func load_items_images() -> void:
 
 func get_random_item_image() -> Dictionary:
 	return _item_images.pick_random()
+	
+func get_image(index: int) -> Dictionary:
+	return _item_images[index]
 
+func get_random_frame_image() -> CompressedTexture2D:
+	return FRAME_IMAGES.pick_random()
+	
+func shuffle_images() -> void:
+	_item_images.shuffle()
