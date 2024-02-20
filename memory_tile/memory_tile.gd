a0ebf40f7e83f13726213d212c724ed56ab38b6e
kgf-cmd <Karolfracek123@gmail.com>
2024-02-20 13:58:20 +0100

    game logic

--- memory_tile/memory_tile.gd
+++ memory_tile/memory_tile.gd
@@ -1,4 +1,8 @@
 extends TextureButton
+
+class_name MemoryTile
+
+
 @onready var frame_image = $FrameImage
 @onready var item_image = $ItemImage
 
@@ -24,6 +28,17 @@ func setup(ii_dict: Dictionary, fi:CompressedTexture2D) -> void:
 	_item_name = ii_dict.item_name
 	reveal(false)
 
+func kill_on_success() -> void:
+	z_index = 1
+	var tween = get_tree().create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(self, "disabled", true, 0)
+	tween.tween_property(self, "rotation", deg_to_rad(720), 0.5)
+	tween.tween_property(self, "scale", Vector2(1.5, 1.5), 0.5)
+	tween.set_parallel(false)
+	tween.tween_interval(0.6)
+	tween.tween_property(self, "scale", Vector2(0, 0), 0)
+
 func on_selection_disabled() ->  void:
 	_can_select_me = false
 	
@@ -32,4 +47,4 @@ func on_selection_enabled() -> void:
 
 func _on_pressed():
 	if _can_select_me == true:
-		reveal(true)
+		SignalManager.on_tile_selected.emit(self)
